278dcb0883750aefbe1288bab357393bf8ec7ef2
Alexandr Hardcore'ov <sharkov.as88@gmail.com>
2022-11-07 13:09:36 +0300

    3.1 HW done!

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Датчик')
+    description = models.CharField(max_length=30, verbose_name='Описание')
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, db_column='sensor', on_delete=models.CASCADE, related_name='measurements', verbose_name='ID датчика')
+    temp = models.FloatField(verbose_name='Температура')
+    measuring_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время замера')
+    photo = models.ImageField(upload_to='measurement/measurements', null=True, blank=True)
--- 3.1-drf-intro/smart_home/smart_home/settings.py
+++ 3.1-drf-intro/smart_home/smart_home/settings.py
@@ -80,9 +80,11 @@ WSGI_APPLICATION = 'smart_home.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_smart_home',
-        'HOST': '127.0.0.1',
+        'NAME': 'first_django',
+        'HOST': 'localhost',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'Qwerty2022',
     }
 }
 
