d9c905228f61d83c870d0d8924a1e0abf4da734c
Alexandr Hardcore'ov <sharkov.as88@gmail.com>
2022-11-07 17:49:09 +0300

    3.2 HW done!

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,29 @@
 from rest_framework import serializers
 
+from .models import Product, StockProduct, Stock
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    title = serializers.CharField(max_length=60)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -24,9 +33,10 @@ class StockSerializer(serializers.ModelSerializer):
         stock = super().create(validated_data)
 
         # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        StockProduct.objects.bulk_create([StockProduct(stock=stock,
+                                                       product=position['product'],
+                                                       quantity=position['quantity'],
+                                                       price=position['price']) for position in positions])
         return stock
 
     def update(self, instance, validated_data):
@@ -36,8 +46,52 @@ class StockSerializer(serializers.ModelSerializer):
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        from rest_framework import serializers
 
-        return stock
+        from .models import Product, StockProduct, Stock
+
+        class ProductSerializer(serializers.ModelSerializer):
+            title = serializers.CharField(max_length=60)
+
+            class Meta:
+                model = Product
+                fields = ['id', 'title', 'description']
+
+        class ProductPositionSerializer(serializers.ModelSerializer):
+
+            class Meta:
+                model = StockProduct
+                fields = ['product', 'quantity', 'price']
+
+        class StockSerializer(serializers.ModelSerializer):
+            positions = ProductPositionSerializer(many=True)
+
+            class Meta:
+                model = Stock
+                fields = ['address', 'positions']
+
+            def validate_positions(self, value):
+                if not value:
+                    raise serializers.ValidationError('Не указаны позиции товара на складе')
+                position_ids = [item['product'].id for item in value]
+                if len(position_ids) != len(set(position_ids)):
+                    raise serializers.ValidationError('Дублируются позиции товара на складе')
+                return value
+
+            def create(self, validated_data):
+                positions = validated_data.pop('positions')
+                stock = super().create(validated_data)
+                StockProduct.objects.bulk_create([StockProduct(stock=stock,
+                                                               product=position['product'],
+                                                               quantity=position['quantity'],
+                                                               price=position['price']) for position in positions])
+                return stock
+
+            def update(self, instance, validated_data):
+                positions = validated_data.pop('positions')
+                stock = super().update(instance, validated_data)
+                for position in positions:
+                    product = position.pop('product')
+                    obj, created = StockProduct.objects.update_or_create(product=product, stock=stock,
+                                                                         defaults=position)
+                return stock
--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'logistic',
 ]
@@ -80,9 +81,11 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_stocks_products',
-        'HOST': '127.0.0.1',
+        'NAME': 'first_django',
+        'HOST': 'localhost',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'Qwerty2022',
     }
 }
 
